84bf22a0945d8890b8e3e20d84844f839a6ee64c
Isaac51743 <m15828509035@163.com>
2020-06-28 17:09:58 -0400

    modified on 06/28/2020

--- src/Recursion/Recursion1.py
+++ src/Recursion/Recursion1.py
@@ -1,49 +1,29 @@
-# 1/8/2020
-# Fibonacci
-# def fibonacci(num):
-#     if num == 0:
-#         return 0
-#     elif num == 1:
-#         return 1
-#     return fibonacci(num - 1) + fibonacci(num - 2)
-
-# 1/30/2020
-# def fibonacci(num):
-#     if num == 0 or num == 1:
-#         return num
-#     else:
-#         result = fibonacci(num - 1) + fibonacci(num - 2)
-#         return result
-# 3/10/2020
-def fibonacci(num):
-    if num == 0 or num == 1:
-        return num
-    return fibonacci(num - 1) + fibonacci(num - 2)
+# 6/28/2020
+def fibonacci(index):
+    if index == 0 or index == 1:
+        return index
+    return fibonacci(index - 1) + fibonacci(index - 2)
 
 print(fibonacci(4))
 
-
-# 2^n, assume: n>=0
-# solution1
-# def power(a, b):
-#     if b == 1:
-#         return a
-#     elif b == 0:
-#         return 1
-#     return a * power(a, b - 1)
-
-# assuming a ≠ 0, b >= 0
-# # solution2
-def power2(a, b):
-    if b == 0:
-        return 1
-    elif b == 1:
-        return a
-    tem = power2(a, b//2)
-    if b % 2 == 0:
-        return tem * tem
+# assuming a and b are both integer, return type is double
+def aPowerB(a, b):
+    if a == 0 and b > 0:
+        return 0
+    elif a == 0 and b <= 0:
+        return None
+    elif a != 0 and b < 0:
+        return 1/aPowerB(a, -b)
     else:
-        return tem * tem * a
+        # base case
+        if b == 1:
+            return a
+        elif b == 0:
+            return 1
+        squareRoot = aPowerB(a, b//2)
+        if b % 2 == 0:
+            return  squareRoot * squareRoot
+        else:
+            return squareRoot * squareRoot * a
 
-result = power2(-2, 3)
-print(result)
+print(aPowerB(-2, -4))

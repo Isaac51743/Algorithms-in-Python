f640cdbcf40d9b4e3247bcbbc9118d41ee75b594
Isaac51743 <m15828509035@163.com>
2020-08-14 10:46:21 -0400

    coding style

--- src/data_structure/data_structure1.py
+++ src/data_structure/data_structure1.py
@@ -1,31 +1,33 @@
+# 08/14/2020
+
+
 class Stack(object):
 
     def __init__(self):
         self.array = []
 
-    def push(self, element): # O(1)
+    def push(self, element):  # O(1)
         self.array.append(element)
 
-    def pop(self): # amortized O(1)
+    def pop(self):  # O(1)
         if len(self.array) == 0:
             print('Stack already empty!')
             return None
-        topElement = self.array.pop()
-        return topElement
+        return self.array.pop()
 
-    def getSize(self):
+    def get_size(self):
         return len(self.array)
 
-    def getTop(self):
+    def get_top(self):
         if len(self.array) == 0:
             print('Stack already empty!')
             return None
         return self.array[-1]
 
-    def isEmpty(self):
+    def is_empty(self):
         return len(self.array) == 0
 
-    def stackPrint(self, reverse=False):  # the bottom is on the left in default
+    def stack_print(self, reverse=False):  # the bottom is on the left in default
         if reverse:
             self.array.reverse()
             print(self.array)
@@ -39,10 +41,10 @@ class Queue(object):
     def __init__(self):
         self.array = []
 
-    def isEmpty(self):
+    def is_empty(self):
         return len(self.array) == 0
 
-    def getSize(self):
+    def get_size(self):
         return len(self.array)
 
     def push(self, element):
@@ -51,136 +53,148 @@ class Queue(object):
     def pop(self):
         if len(self.array) == 0:
             return None
-        result = self.array.pop(0)
-        return result
+        return self.array.pop(0)
 
-    def queuePrint(self):
+    def queue_print(self):
         print(self.array)
 
 
 stack1 = Stack()
-for element in range(10):
-    stack1.push(element)
-stack1.stackPrint()
-while not stack1.isEmpty():
+queue1 = Queue()
+for num in range(10):
+    stack1.push(num)
+    queue1.push(num)
+stack1.stack_print()
+queue1.queue_print()
+while not stack1.is_empty():
     print(stack1.pop(), end=' ')
 print()
-# ----------------------------------------------------------------------
+while not queue1.is_empty():
+    print(queue1.pop(), end=' ')
+print()
+
+
 class QueueOfTwoStack(object):
 
     def __init__(self):
-        self.leftStack = Stack()
-        self.rightStack = Stack()
+        self.left_stack = []
+        self.right_stack = []
 
     def push(self, element):
-        self.leftStack.push(element)
+        self.right_stack.append(element)
 
     def pop(self):
-        if self.rightStack.isEmpty():
-            if self.leftStack.isEmpty():
-                print('queue already empty')
-                return None
-            else:
-                while not self.leftStack.isEmpty():
-                    self.rightStack.push(self.leftStack.pop())
-        return self.rightStack.pop()
+        if len(self.left_stack) == 0 and len(self.right_stack) == 0:
+            return None
+        elif len(self.left_stack) == 0:
+            while len(self.right_stack) > 0:
+                self.left_stack.append(self.right_stack.pop())
+            return self.left_stack.pop()
+        else:
+            return self.left_stack.pop()
 
-    def queuePrint(self):
-        self.leftStack.stackPrint(reverse = True)
-        self.rightStack.stackPrint()
+    def queue_print(self):
+        self.left_stack.reverse()
+        print(self.left_stack, end=' ')
+        self.right_stack.reverse()
+        print(self.right_stack)
 
-    def isEmpty(self):
-        return self.leftStack.isEmpty() and self.rightStack.isEmpty()
+    def is_empty(self):
+        return len(self.left_stack) == 0 and len(self.right_stack) == 0
 
 
-queue1 = QueueOfTwoStack()
-for element in range(10):
-    queue1.push(element)
-queue1.queuePrint()
-while not queue1.isEmpty():
-    print(queue1.pop(), end=' ')
+queue2 = QueueOfTwoStack()
+for num in range(10):
+    queue2.push(num)
+queue2.queue_print()
+while not queue2.is_empty():
+    print(queue2.pop(), end=' ')
 print()
-# ----------------------------------------------------------------------
+
+
 class StackWithMin1():
 
     def __init__(self):
-        self.stack = Stack()
-        self.currentMin = Stack()
+        self.stack = []
+        self.cur_min = []
 
     def push(self, element):
-        self.stack.push(element)
-        if self.currentMin.isEmpty() or self.currentMin.getTop() > element:
-            self.currentMin.push(element)
+        self.stack.append(element)
+        if len(self.cur_min) == 0 or element < self.cur_min[-1]:
+            self.cur_min.append(element)
         else:
-            self.currentMin.push(self.currentMin.getTop())
+            self.cur_min.append(self.cur_min[-1])
 
     def pop(self):
-        self.currentMin.pop()
+        self.cur_min.pop()
         return self.stack.pop()
 
-    def getMin(self):
-        return self.currentMin.getTop()
+    def get_min(self):
+        return self.cur_min[-1] if len(self.cur_min) > 0 else None
 
-    def getSize(self):
-        return self.stack.getSize()
+    def get_size(self):
+        return len(self.stack)
 
-    def getTop(self):
-        return self.stack.getTop()
+    def get_top(self):
+        return self.stack[-1] if len(self.stack) > 0 else None
 
-    def isEmpty(self):
-        return self.stack.isEmpty()
+    def is_empty(self):
+        return len(self.stack) == 0
 
-    def stackPrint(self, reverse = False):
-        self.stack.stackPrint(reverse)
-        self.currentMin.stackPrint(reverse)
+    def stack_print(self):
+        print(self.stack)
+        print(self.cur_min)
 
 
 class StackWithMin2():
 
     def __init__(self):
-        self.stack = Stack()
-        # currentMin stores tuple having 2 elements,
+        self.stack = []
+        # current_min stores tuples with 2 elements
         # first represents size, second represents the minimum value
-        self.currentMin = Stack()
+        self.cur_min = []
 
     def push(self, element):
-        self.stack.push(element)
-        if self.currentMin.isEmpty() or self.currentMin.getTop()[1] > element:
-            self.currentMin.push((self.stack.getSize(), element))
+        self.stack.append(element)
+        if len(self.cur_min) == 0 or element < self.cur_min[-1][-1]:
+            self.cur_min.append((len(self.stack), element))
 
     def pop(self):
-        if self.currentMin.getSize() != 0 and self.currentMin.getTop()[0] == self.stack.getSize():
-            self.currentMin.pop()
+        if len(self.cur_min) > 0 and len(self.stack) == self.cur_min[-1][0]:
+            self.cur_min.pop()
         return self.stack.pop()
 
-    def getMin(self):
-        if not self.currentMin.isEmpty():
-            return self.currentMin.getTop()[1]
-        else:
-            return None
+    def get_min(self):
+        return self.cur_min[-1][-1] if len(self.cur_min) > 0 else None
 
-    def getSize(self):
-        return self.stack.getSize()
+    def get_size(self):
+        return len(self.stack)
 
-    def getTop(self):
-        return self.stack.getTop()
+    def get_top(self):
+        return self.stack[-1] if len(self.stack) > 0 else None
 
-    def isEmpty(self):
-        return self.stack.isEmpty()
+    def is_empty(self):
+        return len(self.stack) == 0
 
-    def stackPrint(self, reverse=False):
-        self.stack.stackPrint(reverse)
-        self.currentMin.stackPrint(reverse)
+    def stack_print(self):
+        print(self.stack)
+        print(self.cur_min)
 
 
 testArray = [6, 6, 3, 6, 3, 3, 1, 2, 3]
 
 stack2 = StackWithMin1()
-for element in testArray:
-    stack2.push(element)
-stack2.stackPrint()
+for num in testArray:
+    stack2.push(num)
+stack2.stack_print()
+while not stack2.is_empty():
+    print(stack2.pop(), end=' ')
+print()
 
 stack3 = StackWithMin2()
-for element in testArray:
-    stack3.push(element)
-stack3.stackPrint()
+for num in testArray:
+    stack3.push(num)
+stack3.stack_print()
+while not stack3.is_empty():
+    print(stack3.pop(), end=' ')
+print()
